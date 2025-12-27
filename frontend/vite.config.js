e7b58681ba9b4f35d3fe45b7a31d4d663cfaabd6
Robiul Hasan <robiulhasan2k25@gmail.com>
2025-12-27 19:57:39 +0600

    styel-wear

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 10000,
-    host: '0.0.0.0'
-  },
-  preview: {
-    port: 10000,
-    host: '0.0.0.0'
-  }
+  server: {port : 5173}
 })
